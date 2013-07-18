c3c2fcb8a27e0074e55151a764c1a7bc7f634a7e
joachim <jv@intox.dk>
2013-07-18 17:06:49 +0200

    templates

---
+++ wp-content/plugins/wpchaos-search/templates/object-page.php
@@ -0,0 +1,12 @@
+<?php
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			material
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
