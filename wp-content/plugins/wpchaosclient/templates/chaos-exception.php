9737d02e9c75761325e6c7bc30f4e535a78716f4
Kræn Hansen <post@creen.dk>
2013-07-23 13:31:24 +0200

    Minor tweaks on the chaos exceptions.

--- wp-content/plugins/wpchaosclient/templates/chaos-exception.php
+++ wp-content/plugins/wpchaosclient/templates/chaos-exception.php
@@ -1,3 +1,4 @@
 <h1 class="text-error">Woups ... Something unexpected happend in the CHAOS:\_</h1>
+<h2 class="text-error">Don't you worrie - this is probably not your fault.</h2>
 <p class="text-error"><?php echo $exception->getMessage() ?></p>
 <pre class="text-error"><?php echo $exception->getTraceAsString() ?></pre>
