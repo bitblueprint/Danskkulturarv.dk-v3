e986c22d752478a4f063c30fd146d6592ce6320a
Mathies Møller Højmose <mathies@traceagency.com>
2013-07-23 14:55:51 +0200

    Working on show object

--- wp-content/themes/dka/templates/multiattrwidget-default.php
+++ wp-content/themes/dka/templates/multiattrwidget-default.php
@@ -5,6 +5,6 @@
  */
 ?>
 <h1><?php echo WPChaosClient::get_object()->title; ?></h1>
-<div class="organization"><strong class="strong orange"><?php echo WPChaosClient::get_object()->organization; ?></strong></div>
+<i class="icon-film"></i>&nbsp;<span class="organization"><strong class="strong orange"><?php echo WPChaosClient::get_object()->organization; ?></strong></span><i class="icon-calendar"></i>
 <p class="date"><?php echo WPChaosClient::get_object()->published; ?></p>
 <div class="description"><?php echo WPChaosClient::get_object()->description; ?></div>
