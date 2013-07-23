ae5637fac037913fa31ca83e4119120ba087b8d0
Mathies Møller Højmose <mathies@traceagency.com>
2013-07-23 22:31:46 +0200

    Worked on responsiveness on search results and item view

--- wp-content/themes/dka/templates/multiattrwidget-default.php
+++ wp-content/themes/dka/templates/multiattrwidget-default.php
@@ -7,4 +7,4 @@
 <div style="width:100%; background-color:#ccff00; height:400px;">Temporary clip placeholder</div>
 <h1><?php echo WPChaosClient::get_object()->title; ?></h1>
 <i class="icon-film"></i>&nbsp;<span class="organization"><strong class="strong orange"><?php echo WPChaosClient::get_object()->organization; ?></strong></span>&nbsp;&nbsp;<i class="icon-calendar"></i>&nbsp;<span class="date"><?php echo WPChaosClient::get_object()->published; ?></span>
-<div class="description"><p><strong>Beskrivelse</strong></p><?php echo WPChaosClient::get_object()->description; ?></div>
+<hr></hr><div class="description"><p><strong>Beskrivelse</strong></p><?php echo WPChaosClient::get_object()->description; ?></div>
