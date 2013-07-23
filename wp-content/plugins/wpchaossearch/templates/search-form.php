67accfc89133d32f6943c9a48996652e0db6c406
Anders Hojmose <anders@traceagency.com>
2013-07-23 13:19:51 +0200

    Worked on the advanced search markup

--- wp-content/plugins/wpchaossearch/templates/search-form.php
+++ wp-content/plugins/wpchaossearch/templates/search-form.php
@@ -10,6 +10,18 @@
 		<input class="span7" id="appendedInputButton" type="text" name="<?php echo WPChaosSearch::QUERY_KEY_FREETEXT; ?>" value="<?php echo $freetext; ?>" placeholder="<?php echo $placeholder; ?>" /><button type="submit" class="btn btn-large btn-search">Søg</button>
 	</div>
 	<div class="btn-group span4 pull-right btn-advanced-search-container">
-		<button class="btn btn-white btn-large btn-block btn-advanced-search collapsed" type="button" data-toggle="collapse" href="#advanced-search-container">Præciser søgning</button>
+		<button class="btn btn-white btn-large btn-block btn-advanced-search" type="button" data-toggle="collapse" href="#advanced-search-container">Præciser søgning</button>
 	</div>
+	<div id="advanced-search-container" class="container row">
+	  
+	    <div class="span3 filter-container filter-media-type" data-toggle="buttons-checkbox">
+	      <button type="button" class="btn filter-btn filter-all" value="dr" name="dr-name">Alle Typer<i class="enabled"></button>
+	      <hr>
+	      <button type="button" class="btn filter-btn" value="dr" name="dr-name"><i class="audio"></i>Lyd<i class="enabled"></button>
+	      <button type="button" class="btn filter-btn"><i class="video"></i>Video<i class="enabled"></i></button>
+	      <button type="button" class="btn filter-btn"><i class="documents"></i>Dokumenter<i class="enabled"></button>
+	      <button type="button" class="btn filter-btn"><i class="images"></i>Billeder<i class="enabled"></button>
+	    </div>
+	  
+	     
 </form>
--- wp-content/themes/dka/js/custom-functions.js
+++ wp-content/themes/dka/js/custom-functions.js
@@ -1,3 +1,11 @@
 /* =============================================================
  * Custom functions goes here
  * ============================================================ */
+
+jQuery(document).ready(function($) {
+
+	$(".btn-group button").click(function () {
+	    $("#buttonvalue").val($(this).text());
+	});
+	
+}); /* end of as page load scripts */
