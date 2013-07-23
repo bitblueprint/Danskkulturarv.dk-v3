e0ec474446f9bbd400bedc561318f84479be5b41
joachim <jv@intox.dk>
2013-07-23 16:43:59 +0200

    player widget template scaffolding

--- wp-content/plugins/wpdkaobject/templates/player-image.php
+++ wp-content/plugins/wpdkaobject/templates/player-image.php
@@ -4,3 +4,11 @@
  * @version 1.0
  */
 ?>
+<?php //Loop through each file and skip those whose format is not image ?>
+<?php foreach(WPChaosClient::get_object()->Files as $file) : if($file->FormatType != 'Image' || $file->FormatID == 10) continue; ?>
+
+<?php var_dump($file); ?>
+
+<strong><?php echo $file->URL; ?></strong>
+
+<?php endforeach; ?>
