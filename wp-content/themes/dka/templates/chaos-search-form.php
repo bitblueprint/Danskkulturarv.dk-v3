459fd176826978b4025cecb30a57245672968e16
joachim <jv@intox.dk>
2013-07-29 12:19:58 +0200

    image-audio format excluded in filter

--- wp-content/themes/dka/templates/chaos-search-form.php
+++ wp-content/themes/dka/templates/chaos-search-form.php
@@ -17,7 +17,7 @@ $advanced_search_expanded = ((!empty($types) || !empty($organizations)) ? " in" 
 			<label class="btn filter-btn filter-btn-all">Alle Typer<i class="icon-ok"></i></label>
 			<hr>
 			<!-- Chage the inline CSS property style="opacity:0.5;" to display: none; when done debugging. -->
-<?php foreach(WPDKAObject::$format_types as $format_type => $args) : ?>
+<?php foreach(WPDKAObject::$format_types as $format_type => $args) : if($format_type == WPDKAObject::TYPE_IMAGE_AUDIO) continue; ?>
 			<label title="<?php echo $args['title']; ?>" for="<?php echo WPDKASearch::QUERY_KEY_TYPE .'-'. $format_type; ?>" class="btn filter-btn filter-btn-single">
 				<input type="checkbox" class="chaos-filter" style="display: none;" name="<?php echo WPDKASearch::QUERY_KEY_TYPE; ?>[]" value="<?php echo $format_type; ?>" id="<?php echo WPDKASearch::QUERY_KEY_TYPE .'-'. $format_type; ?>" <?php checked(in_array($format_type,(array)$types)); ?>>
 				<i class="<?php echo $args['class']; ?>"></i><?php echo $args['title']; ?><i class="icon-remove-sign"></i>
--- wp-content/themes/dka/templates/multiattrwidget-default.php
+++ wp-content/themes/dka/templates/multiattrwidget-default.php
@@ -4,7 +4,17 @@
  * @subpackage DKA
  */
 ?>
-<div style="width:100%; background-color:#ccff00; height:400px;">Temporary clip placeholder</div>
 <h1><?php echo WPChaosClient::get_object()->title; ?></h1>
-<i class="icon-film"></i>&nbsp;<span class="organization"><strong class="strong orange"><?php echo WPChaosClient::get_object()->organization; ?></strong></span>&nbsp;&nbsp;<i class="icon-calendar"></i>&nbsp;<span class="date"><?php echo WPChaosClient::get_object()->published; ?></span>
-<hr></hr><div class="description"><p><strong>Beskrivelse</strong></p><?php echo WPChaosClient::get_object()->description; ?></div>
+<i title="<?php echo WPChaosClient::get_object()->type_title; ?>" class="<?php echo WPChaosClient::get_object()->type_class; ?>"></i>&nbsp;
+<span class="organization">
+	<strong class="strong orange">
+		<?php echo WPChaosClient::get_object()->organization; ?>
+	</strong>
+</span>&nbsp;&nbsp;
+<i class="icon-calendar"></i>&nbsp;
+<span class="date"><?php echo WPChaosClient::get_object()->published; ?></span>
+<hr></hr>
+<div class="description">
+	<p><strong>Beskrivelse</strong></p>
+	<?php echo WPChaosClient::get_object()->description; ?>
+</div>
