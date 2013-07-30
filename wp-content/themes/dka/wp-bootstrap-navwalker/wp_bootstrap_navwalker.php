61474c45fedf2e144d29f5c2787d9d8f588e2161
Mathies Møller Højmose <mathies@traceagency.com>
2013-07-30 22:54:48 +0200

    Started working on the main menu

--- wp-content/themes/dka/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php
+++ wp-content/themes/dka/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php
@@ -95,7 +95,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 			}
 			
 			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-			$item_output .= ($args->has_children && $depth == 0) ? ' <span class="caret"></span></a>' : '</a>';
+			$item_output .= ($args->has_children && $depth == 0) ? ' <i class="icon-caret-down"></i></a>' : '</a>';
 			$item_output .= $args->after;
 
 			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
