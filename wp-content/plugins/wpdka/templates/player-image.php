d729ed279b471805fbafb44ef007ec1a5b84462e
joachim <jv@intox.dk>
2013-07-31 11:07:46 +0200

    some seo for images

--- wp-content/plugins/wpdka/templates/player-image.php
+++ wp-content/plugins/wpdka/templates/player-image.php
@@ -7,11 +7,14 @@
 <?php //Loop through each file and skip those whose format is not image ?>
 <div class="flexslider">
 	<ul class="slides">
-<?php foreach(WPChaosClient::get_object()->Files as $file) : if($file->FormatType != 'Image' || $file->FormatID == 10) continue; ?>
+<?php foreach(WPChaosClient::get_object()->Files as $file) :
+	if($file->FormatType != 'Image' || $file->FormatID == 10) continue;
+	$title = esc_attr('Billede '.$file->Filename.' til '.WPChaosClient::get_object()->title);
+?>
 		<li>
-			<img src="<?php echo $file->URL; ?>" />
+			<img src="<?php echo $file->URL; ?>" title="<?php echo $title; ?>" alt="<?php echo $title; ?>">
 		</li>
-<?php endforeach; ?>
+<?php ;endforeach; ?>
 	</ul>
 </div>
   
