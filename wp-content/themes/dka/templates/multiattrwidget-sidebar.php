1cd0755f9c309495aa1a8e47fe639e29868d9e82
Mathies Møller Højmose <mathies@traceagency.com>
2013-08-01 22:02:38 +0200

    Working on the sidebar widget

--- wp-content/themes/dka/templates/multiattrwidget-sidebar.php
+++ wp-content/themes/dka/templates/multiattrwidget-sidebar.php
@@ -8,8 +8,11 @@
 	<li>
 		<div><i class="icon-eye-open"></i> Visninger <strong class="pull-right"><?php echo WPChaosClient::get_object()->views; ?></strong></div>
 		<div><i class="icon-external-link"></i> <a target="_blank" href="<?php echo WPChaosClient::get_object()->externalurl; ?>" title="Læs original artikel om <?php echo WPChaosClient::get_object()->title; ?>">Læs original artikel</a></div>
+		<hr>
 		<div><?php echo WPChaosClient::get_object()->rights; ?></div>
+		<hr>
 		<div class="social"><?php dka_social_share(array("link"=>WPChaosClient::get_object()->url)); ?></div>
+		<hr>
 	</li>
 	<li>
 		<h4>Tags</h4>
