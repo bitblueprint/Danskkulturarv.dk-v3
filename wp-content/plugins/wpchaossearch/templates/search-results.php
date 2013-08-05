eebeb641142779f6a0486f99da45b35f6e7ba282
Mathies Møller Højmose <mathies@traceagency.com>
2013-08-05 23:14:01 +0200

    Started marking up view toggle + sort dropdown

--- wp-content/plugins/wpchaossearch/templates/search-results.php
+++ wp-content/plugins/wpchaossearch/templates/search-results.php
@@ -7,10 +7,23 @@
 <?php get_header(); ?>
 <article class="container search-results">
 	<div class="row search-results-top">
-		<div class="col-6">
+		<div class="col-4">
 			<p><span class="hidden-sm">Søgningen på <strong class="blue"><?php echo WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_FREETEXT, 'esc_html'); ?></strong> gav&nbsp;</span><span><?php echo WPChaosSearch::get_search_results()->MCM()->TotalCount(); ?> resultater</span></p>
 		</div>
-		<div class="col-6">
+		<div class="col-2">	
+			<div class="dropdown sortby-dropdown pull-right">
+				  <a class="sortby-link" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#">Sorter efter <strong class="blue">relevans</strong>&nbsp;<i class="icon-caret-down"></i></a>
+				  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+				    <li><a tabindex="-1" href="#">Relevans</a></li>
+				    <li><a tabindex="-1" href="#">Alfabetisk</a></li>
+				    <li><a tabindex="-1" href="#">Antal visninger</a></li>
+				  </ul>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="search-result-listing btn-group"><button type="button" class="btn btn-default active"><i class="icon-th"></i></button><button type="button" class="btn btn-default"><i class="icon-th-list"></i></button></div>
+		</div>
+		<div class="col-4">
 		<ul class="pagination pagination-large pull-right">
 		  <?php echo $pagination = WPChaosSearch::paginate('echo=0&before=&after='); ?>
 		</ul>
