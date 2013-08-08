d0dcaa3c28a90520fd5d91f3ab9c6df68329bbf5
Mathies Møller Højmose <mathies@traceagency.com>
2013-08-08 13:34:26 +0200

    Fixed social icons hover and working on front page

--- wp-content/themes/dka/page.php
+++ wp-content/themes/dka/page.php
@@ -8,12 +8,13 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 			
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
+					<!--<header class="entry-header">
 						<?php the_post_thumbnail(); ?>
 						<h1 class="entry-title">
 							<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'dka' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 						</h1>
 					</header><!-- .entry-header -->
+					
 
 					<div class="entry-content">
 						<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'dka' ) ); ?>
