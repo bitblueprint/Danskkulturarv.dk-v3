714b69ca2f78e18c41f623ba37f5a0e07837b83b
Kræn Hansen (for DR A&R Innovation) <kh@bitblueprint.com>
2013-09-24 15:21:53 +0200

    Adding a utility to get organizations rights as json from a google document.

---
+++ utilities/organization-rights-json.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+$url = 'https://docs.google.com/spreadsheet/pub?key=0AhhyYX-BXbrFdGlHRGY0NGdxazdHMXR2SjJ6aXBXR1E&output=csv';
+$content = file_get_contents($url);
+$raw_rows = explode("\n", $content);
+$rows = array();
+// Don't skip the 0-th row.
+for($r = 0; $r < count($raw_rows); $r++) {
+	$row = $raw_rows[$r];
+	$cells = array();
+	$current_cell = '';
+	$in_quotations = false;
+	for($c = 0; $c < strlen($row); $c++) {
+		$char = $row[$c];
+		if($in_quotations === false && $char == ',') {
+			// New cell ..
+			$cells[] = $current_cell;
+			$current_cell = '';
+		} elseif($in_quotations === false && $char == '"') {
+			$in_quotations = true;
+		} elseif($in_quotations === true && $char == '"') {
+			$in_quotations = false;
+		} else {
+			$current_cell .= $row[$c];
+		}
+	}
+	$rows[] = $cells;
+}
+echo json_encode($rows);
